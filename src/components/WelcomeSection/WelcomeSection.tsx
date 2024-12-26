652f4801741881be8e40212330aae9d63b4dda41
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-12-26 22:26:12 +0200

    refactoring

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -1,27 +1,15 @@
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo.tsx";
 import css from "./WelcomeSection.module.css";
-import useResponsive from "../../hooks/useResponsive.ts";
 import clsx from "clsx";
 import Section from "../Section/Section.tsx";
 import Container from "../Container/Container.tsx";
 
 const WelcomeSection = () => {
-  const { isTablet, isDesktop } = useResponsive();
-
-  let marginBottom: string;
-  if (isDesktop) {
-    marginBottom = "132px";
-  } else if (isTablet) {
-    marginBottom = "50px";
-  } else {
-    marginBottom = "74px";
-  }
-
   return (
     <Section className={clsx(css.section, css.welcomeSection)}>
       <Container className={clsx(css.container, css.welcomeContainer)}>
-        <Logo marginBottom={marginBottom} />
+        <Logo className={css.logo} />
         <div className={css.wrapper}>
           <p className={css.textHomePage}>
             Record daily water intake and track
