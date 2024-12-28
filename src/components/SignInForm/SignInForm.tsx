31fce6f6497736c2bebb2495df69cea4d7423c03
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-12-28 22:35:37 +0200

    add styles for mobile

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -3,6 +3,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useId } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import * as yup from "yup";
+import css from "./SignInForm.module.css";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
 
 type Inputs = {
   email: string;
@@ -37,27 +40,51 @@ const SignInForm = () => {
   console.log(watch("email")); // watch input value by passing the name of it
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <p>Sign In</p>
-      <label htmlFor={emailId}>Email</label>
-      <input
-        id={emailId}
-        placeholder="Enter your email"
-        {...register("email")}
-      />
-      <span>{errors.email?.message}</span>
+    <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
+      <p className={css.title}>Sign In</p>
 
-      <label htmlFor={passwordId}>Password</label>
-      <input
-        id={passwordId}
-        type="password"
-        placeholder="Enter your password"
-        {...register("password")}
-      />
-      <span>{errors.password?.message}</span>
+      <div className={clsx(css.wrapperLabel, css.wrapperLabelEmail)}>
+        <label className={css.label} htmlFor={emailId}>
+          Email
+        </label>
 
-      <button type="submit">Sign In</button>
-      <p>Don&#x2019;t have an account? Sign Up</p>
+        <div className={css.wrapperInput}>
+          <input
+            className={css.input}
+            id={emailId}
+            placeholder="Enter your email"
+            {...register("email")}
+          />
+          <span className={css.error}>{errors.email?.message}</span>
+        </div>
+      </div>
+
+      <div className={clsx(css.wrapperLabel, css.wrapperLabelPassword)}>
+        <label className={css.label} htmlFor={passwordId}>
+          Password
+        </label>
+
+        <div className={css.wrapperInput}>
+          <input
+            className={css.input}
+            id={passwordId}
+            type="password"
+            placeholder="Enter your password"
+            {...register("password")}
+          />
+          <span className={css.error}>{errors.password?.message}</span>
+        </div>
+      </div>
+
+      <button className={css.button} type="submit">
+        Sign In
+      </button>
+      <p className={css.text}>
+        Don&#x2019;t have an account?&#x20;
+        <Link to="/signup" className={css.link}>
+          Sign Up
+        </Link>
+      </p>
     </form>
   );
 };
