02d582b17ad4f2942331472a4b9b9dcd625c3da1
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-12-28 18:55:38 +0200

    add validation schema

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,11 +1,26 @@
 // import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useId } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
 
 type Inputs = {
   email: string;
   password: string;
 };
+
+const SignInValidationSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Incorrect email address")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(8, "The password must be at least 8 characters long")
+      .required("Password is required"),
+  })
+  .required();
 
 const SignInForm = () => {
   const emailId = useId();
@@ -16,7 +31,7 @@ const SignInForm = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ resolver: yupResolver(SignInValidationSchema) });
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   console.log(watch("email")); // watch input value by passing the name of it
@@ -24,19 +39,22 @@ const SignInForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <p>Sign In</p>
-      {/* register your input into the hook by invoking the "register" function */}
       <label htmlFor={emailId}>Email</label>
       <input
         id={emailId}
         placeholder="Enter your email"
         {...register("email")}
       />
+      <span>{errors.email?.message}</span>
 
-      {/* include validation with required or other standard HTML validation rules */}
       <label htmlFor={passwordId}>Password</label>
-      <input id={passwordId} {...register("password", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.password && <span>This field is required</span>}
+      <input
+        id={passwordId}
+        type="password"
+        placeholder="Enter your password"
+        {...register("password")}
+      />
+      <span>{errors.password?.message}</span>
 
       <button type="submit">Sign In</button>
       <p>Don&#x2019;t have an account? Sign Up</p>
