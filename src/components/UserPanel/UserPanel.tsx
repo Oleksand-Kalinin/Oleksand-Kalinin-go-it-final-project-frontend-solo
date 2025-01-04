cf99ca0031460bffbf5833b97bbcfc02f22e975d
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-04 19:42:04 +0200

    add structure UserPanel

--- src/components/UserPanel/UserPanel.tsx
+++ src/components/UserPanel/UserPanel.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import UserBar from "../UserBar/UserBar.tsx";
 import css from "./UserPanel.module.css";
+import UserBarPopover from "../UserBarPopover/UserBarPopover.tsx";
 
 const UserPanel = () => {
+  const [showUserBarPopover, setShowUserBarPopover] = useState(false);
   return (
     <div className={css.wrapperUserPanel}>
       <p className={css.titleUserPanel}>
         Hello
         <span className={css.nameAcceptWeight}>, Nadia!</span>
       </p>
+      <div className={css.wrapperUserBar}>
+        <UserBar />
+        {showUserBarPopover && <UserBarPopover />}
+      </div>
     </div>
   );
 };
