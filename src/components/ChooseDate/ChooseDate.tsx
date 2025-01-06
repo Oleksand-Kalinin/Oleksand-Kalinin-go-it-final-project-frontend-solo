c37d17cbae7ec82153da0ae948d1984e703e7bab
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-06 11:10:44 +0200

    add ChooseDate

---
+++ src/components/ChooseDate/ChooseDate.tsx
@@ -0,0 +1,7 @@
+import css from "./ChooseDate.module.css";
+
+const ChooseDate = () => {
+  return <p className={css.currentDate}>Today</p>;
+};
+
+export default ChooseDate;
--- src/components/DailyInfo/DailyInfo.tsx
+++ src/components/DailyInfo/DailyInfo.tsx
@@ -1,5 +1,18 @@
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn.tsx";
+import ChooseDate from "../ChooseDate/ChooseDate.tsx";
+import css from "./DailyInfo.module.css";
+
 const DailyInfo = () => {
-  return <div>DailyInfo</div>;
+  return (
+    <div className={css.wrapper}>
+      <div className={css.wrapperHeader}>
+        <ChooseDate />
+        <AddWaterBtn sectionName="DailyInfo" />
+      </div>
+
+      <div>WaterList</div>
+    </div>
+  );
 };
 
 export default DailyInfo;
