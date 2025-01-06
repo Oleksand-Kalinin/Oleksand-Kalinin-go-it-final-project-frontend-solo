4ad4d4b593c9a88492e809d33581d56cae34a5f0
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-06 18:50:23 +0200

    add WaterItem

---
+++ src/components/WaterItem/WaterItem.tsx
@@ -0,0 +1,30 @@
+import sprite from "../../images/sprite.svg";
+import css from "./WaterItem.module.css";
+
+const WaterItem = () => {
+  return (
+    <li className={css.item}>
+      <svg className={css.iconWater}>
+        <use href={`${sprite}#mage_water-glass-fill-icon`}></use>
+      </svg>
+      <div className={css.info}>
+        <p className={css.amountWater}>250 ml</p>
+        <p className={css.time}>7:00</p>
+      </div>
+      <div className={css.wrapperButtons}>
+        <button className={css.btn} type="button">
+          <svg className={css.btnIcon}>
+            <use href={`${sprite}#edit-icon`}></use>
+          </svg>
+        </button>
+        <button className={css.btn} type="button">
+          <svg className={css.btnIcon}>
+            <use href={`${sprite}#delete-icon`}></use>
+          </svg>
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default WaterItem;
--- src/components/WaterList/WaterList.tsx
+++ src/components/WaterList/WaterList.tsx
@@ -1,12 +1,13 @@
+import WaterItem from "../WaterItem/WaterItem.tsx";
 import css from "./WaterList.module.css";
 
 const WaterList = () => {
   return (
     <div className={css.wrapper}>
       <ul className={css.list}>
-        <li></li>
-        <li></li>
-        <li></li>
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
       </ul>
       <p>Slider</p>
     </div>
