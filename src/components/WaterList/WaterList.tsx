7ededf5dc8f3c8f52702acb79d510446c5a9b0d2
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-07 12:02:09 +0200

    add slider for WaterList

--- src/components/WaterList/WaterList.tsx
+++ src/components/WaterList/WaterList.tsx
@@ -1,15 +1,46 @@
 import WaterItem from "../WaterItem/WaterItem.tsx";
+import { Swiper, SwiperSlide } from "swiper/react";
 import css from "./WaterList.module.css";
+import "swiper/css";
+// import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Pagination, Scrollbar } from "swiper/modules";
 
 const WaterList = () => {
   return (
     <div className={css.wrapper}>
-      <ul className={css.list}>
-        <WaterItem />
-        <WaterItem />
-        <WaterItem />
-      </ul>
-      <p>Slider</p>
+      <Swiper
+        wrapperTag="ul"
+        className={css.list}
+        modules={[Scrollbar, Pagination]}
+        slidesPerView={2}
+        scrollbar={{ draggable: true }}
+        style={{ paddingBottom: "24px", zIndex: 0 }}
+        spaceBetween={8}
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 32,
+          },
+          1440: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+          },
+        }}
+      >
+        <SwiperSlide tag="li" style={{ display: "flex" }} className={css.item}>
+          <WaterItem />
+        </SwiperSlide>
+        <SwiperSlide tag="li" style={{ display: "flex" }} className={css.item}>
+          <WaterItem />
+        </SwiperSlide>
+        <SwiperSlide tag="li" style={{ display: "flex" }} className={css.item}>
+          <WaterItem />
+        </SwiperSlide>
+        <SwiperSlide tag="li" style={{ display: "flex" }} className={css.item}>
+          <WaterItem />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
